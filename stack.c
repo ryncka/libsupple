e86bf0cad28a2d67309c7006933144e7a44a2e22
ryncka <ryncka@outlook.com>
2024-06-18 15:52:00 +0800

    refactor: remove dynamic_stack

--- stack.c
+++ stack.c
@@ -9,10 +9,6 @@ struct stack {
   uintptr_t** elements;
 };
 
-struct dynamic_stack {
-  stack_t stack;
-};
-
 stack_t stack_new(const uint32_t length) {
   stack_t stack = (stack_t)malloc(sizeof(struct stack));
   if (stack == nullptr) {
@@ -37,6 +33,26 @@ void stack_destroy(stack_t self) {
 bool stack_push(stack_t self, uintptr_t* item) {
   if (stack_full(self)) {
     return false;
+  }
+  self->top++;
+  self->elements[self->top] = item;
+  return true;
+}
+
+bool stack_push_dynamic(stack_t self, uintptr_t* item) {
+  if (stack_full(self)) {
+    const uint32_t new_length   = self->length * 2;
+    uintptr_t**    new_elements = malloc(sizeof(uintptr_t*) * new_length);
+    if (new_elements == nullptr) {
+      return false;
+    }
+    // Copy old-element to new-element
+    for (uint32_t i = 0; i < self->length; i++) {
+      new_elements[i] = self->elements[i];
+    }
+    free(self->elements);
+    self->length   = new_length;
+    self->elements = new_elements;
   }
   self->top++;
   self->elements[self->top] = item;
@@ -69,57 +85,4 @@ bool stack_empty(const stack_t self) {
 
 int64_t stack_length(const stack_t self) {
   return self->top + 1;
-}
-
-dynstack_t dynstack_new(const uint32_t initial_length) {
-  dynstack_t dynstack = malloc(sizeof(struct dynamic_stack));
-  if (dynstack == nullptr) {
-    return nullptr;
-  }
-  stack_t stack = stack_new(initial_length);
-  if (stack == nullptr) {
-    free(dynstack);
-    return nullptr;
-  }
-  dynstack->stack = stack;
-  return dynstack;
-}
-
-void dynstack_destroy(dynstack_t self) {
-  stack_destroy(self->stack);
-  free(self);
-}
-
-bool dynstack_push(dynstack_t self, uintptr_t* item) {
-  if (stack_full(self->stack)) {
-    uint32_t    old_lenght   = self->stack->length;
-    uint32_t    new_length   = old_lenght * 2;
-    uintptr_t** new_elements = malloc(sizeof(uintptr_t*) * new_length);
-    if (new_elements == nullptr) {
-      return false;
-    }
-    for (uint32_t i = 0; i < old_lenght; i++) {
-      new_elements[i] = self->stack->elements[i];
-    }
-    free(self->stack->elements);
-    self->stack->length   = new_length;
-    self->stack->elements = new_elements;
-  }
-  return stack_push(self->stack, item);
-}
-
-uintptr_t* dynstack_peek(const dynstack_t self) {
-  return stack_peek(self->stack);
-}
-
-uintptr_t* dynstack_t_pop(dynstack_t self) {
-  return stack_pop(self->stack);
-}
-
-bool dynstack_empty(const dynstack_t self) {
-  return stack_empty(self->stack);
-}
-
-int64_t dynstack_length(const dynstack_t self) {
-  return stack_length(self->stack);
 }
